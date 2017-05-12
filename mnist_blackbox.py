cb2263e13cb0a1f6e4aedab4f4221b9a1a413998
Juang, Yi-Lin <b02901026@ntu.edu.tw>
2017-05-12 18:25:05 +0800

    2017.05.12 update

--- mnist_blackbox.py
+++ mnist_blackbox.py
@@ -10,67 +10,99 @@ import sys
 
 import keras
 from keras import backend
-from keras.utils.np_utils import to_categorical
+from keras.layers import Activation, Dense, Dropout, Flatten
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Activation, Dropout
+from keras.utils.np_utils import to_categorical
 
 import tensorflow as tf
-from tensorflow.python.platform import app
-from tensorflow.python.platform import flags
+from tensorflow.python.platform import app, flags
 
+from cleverhans.attacks import FastGradientMethod
+from cleverhans.attacks_tf import jacobian_augmentation, jacobian_graph
 from cleverhans.utils import cnn_model
 from cleverhans.utils_mnist import data_mnist
-from cleverhans.utils_tf import model_train, model_eval, batch_eval
-from cleverhans.attacks import fgsm
-from cleverhans.attacks_tf import jacobian_graph, jacobian_augmentation
+from cleverhans.utils_tf import batch_eval, model_eval, model_train
 
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 
-# Argparse
-def is_boost():
-    return args.adaboost or args.gradboost or args.randforest
+# Argparse.
+def is_not_nn():
+    return args.alg != "cnn"
 
-def check_args():
-    if args.max_dep is None or args.n_est is None:
-        print("Error: max_dep, n_est required.")
-        sys.exit(1)
-parser = argparse.ArgumentParser(description="Black-box attack against CNN/Adaptive Boosting/Gradient Boosting/Random Forest.")
-classifier = parser.add_mutually_exclusive_group(required=True)
-classifier.add_argument('--cnn', '-c', action='store_true', help="use cnn as black-box")
-classifier.add_argument('--adaboost', '-a', action='store_true', help="use adaptive boosting as black-box")
-classifier.add_argument('--gradboost', '-g', action='store_true', help="use gradient boosting as black-box")
-classifier.add_argument('--randforest', '-r', action='store_true', help="use random forest as black-box")
-parser.add_argument("--max_dep", "-d", type=int,
-        help="the max depth of decision tree in boosting")
-parser.add_argument("--n_est", "-n", type=int,
-        help="the number of estimators in boosting")
+
+parser = argparse.ArgumentParser(
+        description= "Black-box attack against CNN / \
+                                               Adaptive Boosting / \
+                                               Gradient Boosting / \
+                                               Random Forest / \
+                                               Support Vector Machine.")
+subparsers = parser.add_subparsers(dest='alg', help="choose black-box type")
+subparsers.required = True
+cnn_parser = subparsers.add_parser('cnn', help="use cnn")
+ada_parser = subparsers.add_parser('adaboost', help="use adaptive boosting")
+grad_parser = subparsers.add_parser('gradboost', help="use gradient boosting")
+rand_parser = subparsers.add_parser('randforest', help="use random forest")
+svm_parser = subparsers.add_parser('svm', help="use svm")
+ada_parser.add_argument("n_est", type=int,
+        help="the number of estimators")
+ada_subparsers = ada_parser.add_subparsers(dest='clsfr',
+        help="choose classifier for adaboost")
+ada_subparsers.required = True
+dt_ada_parser = ada_subparsers.add_parser('dt', help="use decision tree")
+dt_ada_parser.add_argument("max_dep", type=int,
+        help="the max depth of decision tree")
+svm_ada_parser = ada_subparsers.add_parser('svm', help="use svm")
+svm_ada_parser.add_argument("--kernel", default='rbf',
+        choices=['linear', 'rbf', 'poly', 'rbf', 'sigmoid', 'precomputed'],
+        help="the kernel type")
+svm_ada_parser.add_argument("C", type=float,
+        help="the penalty parameter C of the error term")
+grad_parser.add_argument("max_dep", type=int,
+        help="the max depth of decision tree")
+grad_parser.add_argument("n_est", type=int,
+        help="the number of estimators")
+rand_parser.add_argument("max_dep", type=int,
+        help="the max depth of decision tree")
+rand_parser.add_argument("n_est", type=int,
+        help="the number of estimators")
+svm_parser.add_argument("--kernel", default='rbf',
+        choices=['linear', 'rbf', 'poly', 'rbf', 'sigmoid', 'precomputed'],
+        help="the kernel type")
+svm_parser.add_argument("C", type=float,
+        help="the penalty parameter C of the error term")
 args = parser.parse_args()
-if args.cnn:
+
+# Generate model name.
+if args.alg == "adaboost":
+    model_name = "adaboost_"
+    if args.clsfr == "dt":
+        model_name += "dep{}_est{}".format(args.max_dep, args.n_est)
+    elif args.clsfr == "svm":
+        model_name += "svm_{}_C{}_est{}".format(args.kernel, args.C, args.n_est)
+    else:
+        print("Error: unknown classifier.")
+        sys.exit(1)
+elif args.alg == "cnn":
     model_name = "cnn"
-elif args.adaboost:
-    check_args()
-    model_name = "adaboost_dep{}_est{}".format(args.max_dep, args.n_est)
-elif args.gradboost:
-    check_args()
-    model_name = "gradboost_dep{}_est{}".format(args.max_dep, args.n_est)
-elif args.randforest:
-    check_args()
-    model_name = "randforest_dep{}_est{}".format(args.max_dep, args.n_est)
+elif args.alg in ["gradboost", "randforest"]:
+    model_name = "{}_dep{}_est{}".format(args.alg, args.max_dep, args.n_est)
+elif args.alg == "svm":
+    model_name = "svm_{}_C{}".format(args.kernel, args.C)
 else:
-    print("Error: unkwown classifier.")
+    print("Error: unknown algorithm.")
     sys.exit(1)
 print(model_name)
 
+# Flags.
 FLAGS = flags.FLAGS
-
 # General flags
 flags.DEFINE_integer('nb_classes', 10, 'Number of classes in problem')
 flags.DEFINE_integer('batch_size', 128, 'Size of training batches')
 flags.DEFINE_float('learning_rate', 0.1, 'Learning rate for training')
-
 # Flags related to substitute
 flags.DEFINE_integer('holdout', 100, 'Test set holdout for adversary')
 flags.DEFINE_integer('data_aug', 6, 'Nb of times substitute data augmented')
@@ -132,32 +164,41 @@ def prep_cnn_bbox(sess, x, y, X_train, Y_train, X_test, Y_test):
                           args=eval_params)
     print("Test accuracy = {}".format(accuracy))
 
-    return predictions
+    return model, predictions
 
 
 def prep_boost_bbox(X_train, Y_train, X_test, Y_test):
-    if args.adaboost:
+    if args.alg == "adaboost":
+        if args.clsfr == "dt":
+            alg = "SAMME.R"
+            classifier = DecisionTreeClassifier(max_depth=args.max_dep)
+        elif args.clsfr == "svm":
+            alg = "SAMME"
+            classifier = SVC(C=args.C, kernel=args.kernel, gamma=0.000005)
         model = AdaBoostClassifier(
-                DecisionTreeClassifier(max_depth=args.max_dep),
-                n_estimators=args.n_est,
-                learning_rate=1.0)
-    elif args.gradboost:
-        model = GradientBoostingClassifier(max_depth=args.max_dep,
+                algorithm=alg,
+                base_estimator=classifier,
+                learning_rate=1.0,
+                n_estimators=args.n_est)
+    elif args.alg == "gradboost":
+        model = GradientBoostingClassifier(loss='deviance', # {‘deviance’, ‘exponential’}
+                                           max_depth=args.max_dep,
                                            n_estimators=args.n_est,
                                            learning_rate=1.0)
-    elif args.randforest:
+    elif args.alg == "randforest":
         model = RandomForestClassifier(max_depth=args.max_dep,
                                        n_estimators=args.n_est,
                                        max_features=None,
                                        n_jobs=-1)
-    else:
-        print("Error: unkwown boosting classifier.")
-        sys.exit(1)
+    elif args.alg == "svm":
+        model = SVC(C=args.C, kernel=args.kernel, gamma=0.000005)
 
     x_train = X_train.reshape(X_train.shape[0], -1)
     y_train = np.argmax(Y_train, axis=1)
     x_test = X_test.reshape(X_test.shape[0], -1)
     y_test = np.argmax(Y_test, axis=1)
+    print("x_train: {}, y_train: {}, x_test: {}, y_test: {}"\
+            .format(x_train.shape, y_train.shape, x_test.shape, y_test.shape))
 
     model.fit(x_train, y_train)
 
@@ -166,10 +207,12 @@ def prep_boost_bbox(X_train, Y_train, X_test, Y_test):
     with open(model_file, "wb") as f:
         pickle.dump(model, f)
     log_file = "log/{}.log".format(model_name)
+    '''
     if getattr(model, "staged_predict", None):
         with open(log_file, "w") as f:
             for predict in model.staged_predict(x_test):
                 f.write("{}\n".format(accuracy_score(predict, y_test)))
+    '''
 
     accuracy = model.score(x_test, y_test)
     print("Test accuracy = {}".format(accuracy))
@@ -245,13 +288,12 @@ def train_substitute(sess, x, y, bbox_preds, X_sub, Y_sub):
         if rho < FLAGS.data_aug - 1:
             # Perform the Jacobian augmentation
             X_sub = jacobian_augmentation(sess, x, X_sub, Y_sub, grads,
-                                          FLAGS.lmbda, keras_phase=keras.
-                                          backend.learning_phase())
+                                          FLAGS.lmbda)
 
             # Label the newly generated synthetic points using the black-box
             Y_sub = np.hstack([Y_sub, Y_sub])
             X_sub_prev = X_sub[int(len(X_sub)/2):]
-            if args.cnn:
+            if args.alg == "cnn":
                 eval_params = {'batch_size': FLAGS.batch_size}
                 bbox_val = batch_eval(sess, [x], [bbox_preds], [X_sub_prev],
                                       args=eval_params)[0]
@@ -259,14 +301,12 @@ def train_substitute(sess, x, y, bbox_preds, X_sub, Y_sub):
                 # only has access to the label (not the probabilities) output
                 # by the black-box model
                 Y_sub_prev = np.argmax(bbox_val, axis=1)
-            elif is_boost():
+            elif is_not_nn():
                 x_sub_prev = X_sub_prev.reshape(X_sub_prev.shape[0], -1)
                 Y_sub_prev = bbox_preds.predict(x_sub_prev)
-            else:
-                print("Error: unknown classifier.")
             Y_sub[int(len(X_sub)/2):] = Y_sub_prev
 
-    return preds_sub
+    return model_sub, preds_sub
 
 
 def main(argv=None):
@@ -274,6 +314,7 @@ def main(argv=None):
     MNIST cleverhans tutorial
     :return:
     """
+    keras.layers.core.K.set_learning_phase(0)
 
     # Perform tutorial setup
     assert setup_tutorial()
@@ -282,6 +323,7 @@ def main(argv=None):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
+    # sess = tf.Session()
     keras.backend.set_session(sess)
 
     # Get MNIST data
@@ -291,6 +333,9 @@ def main(argv=None):
     X_sub = X_test[:FLAGS.holdout]
     Y_sub = np.argmax(Y_test[:FLAGS.holdout], axis=1)
 
+    # Shrink training data.
+    # X_train = X_train[:10000]
+    # Y_train = Y_train[:10000]
     # Redefine test set as remaining samples unavailable to adversaries
     X_test = X_test[FLAGS.holdout:]
     Y_test = Y_test[FLAGS.holdout:]
@@ -300,22 +345,23 @@ def main(argv=None):
     y = tf.placeholder(tf.float32, shape=(None, 10))
 
     print("Preparing the black-box model.")
-    if args.cnn:
-        bbox = prep_cnn_bbox(sess, x, y, X_train, Y_train, X_test, Y_test)
-    elif is_boost():
+    if args.alg == "cnn":
+        model, bbox = prep_cnn_bbox(sess, x, y, X_train, Y_train, X_test, Y_test)
+    elif is_not_nn():
         bbox = prep_boost_bbox(X_train, Y_train, X_test, Y_test)
-    else:
-        print("Error: unknown classifier.")
-        sys.exit(1)
 
     print("Training the substitute model.")
-    substitute_preds = train_substitute(sess, x, y, bbox, X_sub, Y_sub)
+    model_sub, preds_sub = train_substitute(sess, x, y, bbox, X_sub, Y_sub)
+
+    # Initialize the Fast Gradient Sign Method (FGSM) attack object.
+    fgsm_par = {'eps': 0.3, 'ord': np.inf, 'clip_min': 0., 'clip_max': 1.}
+    fgsm = FastGradientMethod(model_sub, sess=sess)
 
     # Craft adversarial examples using the substitute
     print("Crafting the adversarial examples.")
-    adv_x = fgsm(x, substitute_preds, eps=0.3)
     eval_params = {'batch_size': FLAGS.batch_size}
-    X_test_adv, = batch_eval(sess, [x], [adv_x], [X_test], args=eval_params)
+    x_adv_sub = fgsm.generate(x, **fgsm_par)
+    X_test_adv, = batch_eval(sess, [x], [x_adv_sub], [X_test], args=eval_params)
 
     # Dump adversarial examples.
     example_file = "example/{}.data".format(model_name)
@@ -323,16 +369,15 @@ def main(argv=None):
         pickle.dump(X_test_adv, f)
 
     # Evaluate the accuracy of the "black-box" model on adversarial examples
-    if args.cnn:
+    if args.alg == "cnn":
+        # accuracy = model_eval(sess, x, y, model(x_adv_sub), X_test, Y_test,
+                              # args=eval_params)
         accuracy = model_eval(sess, x, y, bbox, X_test_adv, Y_test,
                               args=eval_params)
-    elif is_boost():
+    elif is_not_nn():
         x_test_adv = X_test_adv.reshape(X_test_adv.shape[0], -1)
         y_test = np.argmax(Y_test, axis=1)
         accuracy = bbox.score(x_test_adv, y_test)
-    else:
-        print("Error: unknown classifier.")
-        sys.exit(1)
 
     print("Test adversarial accuracy = {}".format(accuracy))
 
